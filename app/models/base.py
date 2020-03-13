195963d3ea6aa7585ead02b0082cb4fcf9eaa48f
lula <bodanli159951@163.com>
2020-03-13 14:14:36 +0800

    refactor: 重构api.doc装饰器
    - 增加全局参数的选择
    - 增加参数位置的选择

--- app/models/base.py
+++ app/models/base.py
@@ -16,154 +16,182 @@ __author__ = 'Allen7D'
 
 
 class SQLAlchemy(_SQLAlchemy):
-	@contextmanager
-	def auto_commit(self):
-		try:
-			yield
-			self.session.commit()  # 事务
-		except Exception as e:
-			self.session.rollback()  # 回滚
-			raise e
+    @contextmanager
+    def auto_commit(self):
+        try:
+            yield
+            self.session.commit()  # 事务
+        except Exception as e:
+            self.session.rollback()  # 回滚
+            raise e
 
 
 class Pagination(_Pagination):
-	def hide(self, *keys):
-		for item in self.items:
-			item.hide(*keys)
-		return self
+    def hide(self, *keys):
+        for item in self.items:
+            item.hide(*keys)
+        return self
 
 
 class Query(BaseQuery):
-	def filter_by(self, **kwargs):
-		if 'status' not in kwargs.keys():
-			kwargs['status'] = 1
-		return super(Query, self).filter_by(**kwargs)
+    def filter_by(self, **kwargs):
+        if 'status' not in kwargs.keys():
+            kwargs['status'] = 1
+        return super(Query, self).filter_by(**kwargs)
 
-	def get_or_404(self, ident, e=None, error_code=None, msg=None):
-		rv = self.get(ident)  # 查询主键
-		if not rv:
-			if e:
-				raise e
-			raise NotFound(error_code=error_code, msg=msg)
-		return rv
+    def get_or_404(self, ident, e=None, error_code=None, msg=None):
+        rv = self.get(ident)  # 查询主键
+        if not rv:
+            if e:
+                raise e
+            raise NotFound(error_code=error_code, msg=msg)
+        return rv
 
-	def first_or_404(self, e=None, error_code=None, msg=None):
-		'''
-		:param e: 异常(exception)
-		:param error_code: 错误码
-		:param msg: 错误信息
-		:return:
-		'''
-		rv = self.first()
-		if not rv:
-			if e:
-				raise e
-			raise NotFound(error_code=error_code, msg=msg)
-		return rv
+    def first_or_404(self, e=None, error_code=None, msg=None):
+        '''
+        :param e: 异常(exception)
+        :param error_code: 错误码
+        :param msg: 错误信息
+        :return:
+        '''
+        rv = self.first()
+        if not rv:
+            if e:
+                raise e
+            raise NotFound(error_code=error_code, msg=msg)
+        return rv
 
-	def all_or_404(self, e=None, error_code=None, msg=None, wrap=''):
-		rv = list(self)
-		if not rv:
-			if e:
-				raise e
-			raise NotFound(error_code=error_code, msg=msg)
-		if wrap:
-			rv = {
-				'{}'.format(wrap): rv
-			}
-		return rv
+    def all_or_404(self, e=None, error_code=None, msg=None, wrap=''):
+        rv = list(self)
+        if not rv:
+            if e:
+                raise e
+            raise NotFound(error_code=error_code, msg=msg)
+        if wrap:
+            rv = {
+                '{}'.format(wrap): rv
+            }
+        return rv
 
-	def all(self):
-		rv = list(self)
-		if not rv:
-			raise NotFound()
-		return rv
+    def all(self):
+        rv = list(self)
+        if not rv:
+            raise NotFound()
+        return rv
 
-	def paginate(self, page=None, per_page=None, error_out=True, max_per_page=None):
-		# 使用paginator记的加上filter_by，用于默认添加status=1
-		paginator = BaseQuery.paginate(self, page=page, per_page=per_page, error_out=error_out,
-									   max_per_page=max_per_page)
-		return Pagination(self,
-						  paginator.page,
-						  paginator.per_page,
-						  paginator.total,
-						  paginator.items
-						  )
+    def paginate(self, page=None, per_page=None, error_out=True, max_per_page=None):
+        # 使用paginator记的加上filter_by，用于默认添加status=1
+        paginator = BaseQuery.paginate(self, page=page, per_page=per_page, error_out=error_out,
+                                       max_per_page=max_per_page)
+        return Pagination(self,
+                          paginator.page,
+                          paginator.per_page,
+                          paginator.total,
+                          paginator.items
+                          )
 
 
 db = SQLAlchemy(query_class=Query)
 
 
+class CRUDMixin(object):
+    """Mixin 添加CRUD操作(create, read, update, delete)."""
+
+    @classmethod
+    def create(cls, **kwargs):
+        """Create 新的记录并保存到数据库"""
+        instance = cls(**kwargs)
+        return instance.save()
+
+    def update(self, commit=True, **kwargs):
+        """Update 指定字段到记录"""
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+        return commit and self.save() or self
+
+    def save(self, commit=True):
+        """Save 记录"""
+        db.session.add(self)
+        if commit:
+            db.session.commit()
+        return self
+
+    def delete(self, commit=True):
+        """Remove 记录从数据库中"""
+        db.session.delete(self)
+        return commit and db.session.commit()
+
+
 class Base(db.Model):
-	__abstract__ = True
-	create_time = Column('create_time', Integer, comment='创建时间')
-	delete_time = Column(Integer, comment='删除时间')
-	update_time = Column(Integer, comment='更新时间')
-	status = Column(SmallInteger, default=1, comment='状态，是否软删除')  # 软删除
+    __abstract__ = True
+    create_time = Column('create_time', Integer, comment='创建时间')
+    delete_time = Column(Integer, comment='删除时间')
+    update_time = Column(Integer, comment='更新时间')
+    status = Column(SmallInteger, default=1, comment='状态，是否软删除')  # 软删除
 
-	@orm.reconstructor
-	def init_on_load(self):
-		# 被隐藏的属性则无法用append方法添加
-		self.exclude = ['create_time', 'update_time', 'delete_time', 'status']
-		all_columns = inspect(self.__class__).columns.keys()
-		self.fields = list(set(all_columns) - set(self.exclude))
+    @orm.reconstructor
+    def init_on_load(self):
+        # 被隐藏的属性则无法用append方法添加
+        self.exclude = ['create_time', 'update_time', 'delete_time', 'status']
+        all_columns = inspect(self.__class__).columns.keys()
+        self.fields = list(set(all_columns) - set(self.exclude))
 
-	def __init__(self):
-		self.create_time = int(datetime.now().timestamp())
+    def __init__(self):
+        self.create_time = int(datetime.now().timestamp())
 
-	def __getitem__(self, item):
-		attr = getattr(self, item)
-		# 将字符串转为JSON
-		if isinstance(attr, str):
-			try:
-				attr = json.loads(attr)
-			except ValueError:
-				pass
-		# 处理时间(时间戳转化)
-		if item in ['create_time', 'update_time', 'delete_time']:
-			attr = strftime('%Y-%m-%d %H:%M:%S', localtime(attr))
-		return attr
+    def __getitem__(self, item):
+        attr = getattr(self, item)
+        # 将字符串转为JSON
+        if isinstance(attr, str):
+            try:
+                attr = json.loads(attr)
+            except ValueError:
+                pass
+        # 处理时间(时间戳转化)
+        if item in ['create_time', 'update_time', 'delete_time']:
+            attr = strftime('%Y-%m-%d %H:%M:%S', localtime(attr))
+        return attr
 
-	@property
-	def create_datetime(self):
-		if self.create_time:
-			return datetime.fromtimestamp(self.create_time)
-		else:
-			return None
+    @property
+    def create_datetime(self):
+        if self.create_time:
+            return datetime.fromtimestamp(self.create_time)
+        else:
+            return None
 
-	def get_url(self, url):
-		if (self._from == 1):
-			return current_app.config['IMG_PREFIX'] + url
-		else:
-			return url
+    def get_url(self, url):
+        if (self._from == 1):
+            return current_app.config['IMG_PREFIX'] + url
+        else:
+            return url
 
-	def set_attrs(self, **kwargs):
-		# 快速赋值
-		# 用法: set_attrs(form.data)
-		for key, value in kwargs.items():
-			if hasattr(self, key) and key != 'id':
-				setattr(self, key, value)
+    def set_attrs(self, **kwargs):
+        # 快速赋值
+        # 用法: set_attrs(form.data)
+        for key, value in kwargs.items():
+            if hasattr(self, key) and key != 'id':
+                setattr(self, key, value)
 
-	def delete(self):
-		self.status = 0
+    def delete(self):
+        self.status = 0
 
-	def keys(self):
-		# 在 app/app.py中的 JSONEncoder中的 dict(o)使用
-		# 在此处，整合要输出的属性：self.fields
-		return self.fields
+    def keys(self):
+        # 在 app/app.py中的 JSONEncoder中的 dict(o)使用
+        # 在此处，整合要输出的属性：self.fields
+        return self.fields
 
-	def hide(self, *keys):
-		for key in keys:
-			self.exclude.append(key)
-			if key in self.fields:
-				self.fields.remove(key)
-		return self
+    def hide(self, *keys):
+        for key in keys:
+            self.exclude.append(key)
+            if key in self.fields:
+                self.fields.remove(key)
+        return self
 
-	def append(self, *keys):
-		for key in keys:
-			# self.fields 暂未有key
-			# and
-			# 在 Model层和 Service层等任意的操作中，已经隐藏的属性无法再添加
-			if key not in self.fields and key not in self.exclude:
-				self.fields.append(key)
-		return self
+    def append(self, *keys):
+        for key in keys:
+            # self.fields 暂未有key
+            # and
+            # 在 Model层和 Service层等任意的操作中，已经隐藏的属性无法再添加
+            if key not in self.fields and key not in self.exclude:
+                self.fields.append(key)
+        return self

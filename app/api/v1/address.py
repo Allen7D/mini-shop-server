73f55419320176291ce6c25a36a3d8a58bd70fd0
lula <bodanli159951@163.com>
2020-03-20 15:35:41 +0800

    feat(api/v1/address.py): 新增create、delete接口
    - 新增create、delete接口
    - 修改视图函数的命名
    - 修改红图的信息

--- app/api/v1/address.py
+++ app/api/v1/address.py
@@ -10,31 +10,67 @@ from app.libs.redprint import RedPrint
 from app.libs.token_auth import auth
 from app.models.user import User
 from app.models.user_address import UserAddress
+from app.validators.base import BaseValidator
 from app.validators.forms import UpdateAddressValidator
 from app.api_docs.v1 import address as api_doc
 
 __author__ = 'Allen7D'
 
-api = RedPrint(name='address', description='用户地址', api_doc=api_doc)
+api = RedPrint(name='address', description='配送信息', api_doc=api_doc)
+
+
+@api.route('/list', methods=['GET'])
+@api.doc(auth=True)
+@auth.login_required
+def get_list():
+    '''获取所有「配送信息」'''
+    uid = g.user.uid
+    user_address = UserAddress.query.filter_by(user_id=uid).all_or_404(
+        error_code=6001, msg='配送地址不存在')
+    return Success(user_address)
+
 
 @api.route('', methods=['GET'])
 @api.doc(auth=True)
 @auth.login_required
-def get_address():
-	'''获取「用户自身的地址」'''
-	uid = g.user.uid
-	user_address = UserAddress.query.filter_by(user_id=uid).first_or_404(
-		error_code=6001, msg='用户地址不存在')
-	return Success(user_address)
+def get_one():
+    '''获取「配送信息」'''
+    uid = g.user.uid
+    user_address = UserAddress.get_or_404(user_id=uid, error_code=6001, msg='配送地址不存在')
+    return Success(user_address)
+
+
+@api.route('', methods=['POST'])
+@api.doc(args=['name', 'mobile', 'province', 'city', 'country', 'detail'], auth=True)
+@auth.login_required
+def create_one():
+    '''新增「配送信息」'''
+    address_info = UpdateAddressValidator().validate_for_api().data
+    uid = g.user.uid
+    user = User.query.filter_by(id=uid).first_or_404(e=UserException)
+    user.save_address(address_info)
+    return Success(error_code=1)
 
 
 @api.route('', methods=['PUT'])
 @api.doc(args=['name', 'mobile', 'province', 'city', 'country', 'detail'], auth=True)
 @auth.login_required
-def update_address():
-	'''更新「用户自身的地址」'''
-	address_info = UpdateAddressValidator().validate_for_api().data
-	uid = g.user.uid
-	user = User.query.filter_by(id=uid).first_or_404(e=UserException)
-	user.save_address(address_info)
-	return Success(error_code=1)
+def update_one():
+    '''更新「配送信息」'''
+    address_info = UpdateAddressValidator().validate_for_api().data
+    uid = g.user.uid
+    user = User.query.filter_by(id=uid).first_or_404(e=UserException)
+    user.save_address(address_info)
+    return Success(error_code=1)
+
+
+@api.route('', methods=['DELETE'])
+@api.doc(args=['*int.path.address_id'], auth=True)
+@auth.login_required
+def delete_one():
+    '''删除「配送信息」'''
+    uid = g.user.uid
+    validator = BaseValidator().get_all_json()
+    user_address = UserAddress.get_or_404(user_id=uid, id=validator.address_id)
+    user_address.delete()
+    return Success(error_code=2)

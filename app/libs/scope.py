03edb452431943b4cb4a2607b9bbd2bb0d9326a9
lula <bodanli159951@163.com>
2020-04-09 01:51:30 +0800

    refactor(models/user.py): 优化User类中的登录校验
    - 使用user.auth_scope代替Scope.match_user_scope(auth=user.auth)

--- app/libs/scope.py
+++ app/libs/scope.py
@@ -21,25 +21,6 @@ class Scope:
 
         return self
 
-    @staticmethod
-    def match_user_scope(auth, type='en'):
-        return 'UserScope'
-        # auth_scope_en = {
-        #     ScopeEnum.USER: 'UserScope',
-        #     ScopeEnum.ADMIN: 'AdminScope',
-        #     ScopeEnum.SUPER: 'SuperScope'
-        # }
-        # auth_scope_cn = {
-        #     ScopeEnum.USER: '普通用户',
-        #     ScopeEnum.ADMIN: '系统管理员',
-        #     ScopeEnum.SUPER: '系统超级管理员'
-        # }
-        # if type == 'en':
-        #     return auth_scope_en.get(ScopeEnum(auth), 'UserScope')
-        # elif type == 'cn':
-        #     return auth_scope_cn.get(ScopeEnum(auth), '普通用户')
-
-
 class UserScope(Scope):
     allow_api = [] + \
                 []

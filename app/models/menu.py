5167f0893a9e98a90f67549cbae9fc23913090c8
Dongwei Dong <bodanli159951@gmail.com>
2020-05-19 09:42:23 +0800

    style(/app/modles/menu.py): 变更代码结构，删除冗余代码

--- app/models/menu.py
+++ app/models/menu.py
@@ -3,16 +3,13 @@
   Created by Mohan on 2020/4.
 """
 from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
 
-from app.core.db import BaseModel as Base
-from app.models.route import Route
-from app.models.group import Group
+from app.core.db import BaseModel as Model
 
 __author__ = 'Mohan'
 
 
-class Menu(Base):
+class Menu(Model):
     __tablename__ = 'menu'
     group_id = Column(Integer, ForeignKey('group.id'), primary_key=True, comment='外键 权限组ID')
     route_id = Column(Integer, ForeignKey('route.id'), primary_key=True, comment='外键 路由节点ID')
